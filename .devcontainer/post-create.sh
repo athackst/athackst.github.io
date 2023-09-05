c86f2ab9b5b04cef45becedf1cb3bfcb1eb067e9
Allison Thackston <allison@allisonthackston.com>
2023-09-04 22:29:09 -0700

    Fix crash on devcontainer postcreate (#183)

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,12 +8,13 @@ echo "Ruby version $ruby_version set in .ruby-version file"
 # Install the version of Bundler.
 if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock >/dev/null; then
     echo "Installing bundler in gemfile"
-    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs sudo gem install bundler -v
+    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
 fi
 
 # If there's a Gemfile, then run `bundle install`
 # It's assumed that the Gemfile will install Jekyll too
 if [ -f Gemfile ]; then
+    echo "installing gems from gemfile"
     bundle config --local path vendor/bundle
     bundle install
 fi
